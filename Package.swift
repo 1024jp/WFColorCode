c6d9cb8ce6306fa79d3347258c28489a32624ec8
1024jp <1024jp@wolfrosch.com>
2019-06-05 15:46:33 -0700

    Fix Package.swift

--- Package.swift
+++ Package.swift
@@ -1,9 +1,11 @@
 // swift-tools-version:5.0
-
 import PackageDescription
 
 let package = Package(
     name: "ColorCode",
+    products: [
+        .library(name: "ColorCode", targets: ["ColorCode"]),
+    ],
     targets: [
         .target(name: "ColorCode"),
         .testTarget(name: "ColorCodeTests", dependencies: ["ColorCode"]),
