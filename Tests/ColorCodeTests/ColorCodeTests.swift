c1e9fe272e7dcc2a3fe0e4ac3cecc7350abf97fb
1024jp <1024jp@wolfrosch.com>
2020-02-27 18:58:57 +0900

    Add final to test class declaration

--- Tests/ColorCodeTests/ColorCodeTests.swift
+++ Tests/ColorCodeTests/ColorCodeTests.swift
@@ -7,7 +7,7 @@
 /*
  The MIT License (MIT)
  
- Copyright (c) 2014-2019 1024jp
+ Copyright (c) 2014-2020 1024jp
  
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
@@ -31,7 +31,7 @@
 import XCTest
 import ColorCode
 
-class ColorCodeTests: XCTestCase {
+final class ColorCodeTests: XCTestCase {
 
     func testColorCreation() {
         
