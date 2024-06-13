98341d17ff6917d7702b72a0f474a4f45e13a3a3
1024jp <1024jp@wolfrosch.com>
2024-06-13 22:43:38 +0900

    Update to Swift 6.0

--- Package.swift
+++ Package.swift
@@ -1,19 +1,17 @@
-// swift-tools-version:5.9
+// swift-tools-version: 6.0
 import PackageDescription
 
 let package = Package(
     name: "ColorCode",
     platforms: [
-        .macOS(.v11),
+        .macOS(.v14),
     ],
     products: [
         .library(name: "ColorCode", targets: ["ColorCode"]),
     ],
     targets: [
-        .target(name: "ColorCode",
-                swiftSettings: [.enableExperimentalFeature("StrictConcurrency")]),
-        .testTarget(name: "ColorCodeTests",
-                    dependencies: ["ColorCode"],
-                    swiftSettings: [.enableExperimentalFeature("StrictConcurrency")]),
-    ]
+        .target(name: "ColorCode"),
+        .testTarget(name: "ColorCodeTests", dependencies: ["ColorCode"])
+    ],
+    swiftLanguageVersions: [.v6]
 )
